c85a6b00357b9791495990f1eee9055e5309040a
prasag1 <guru.prasad@bsci.com>
2023-07-29 11:57:24 +0530

    Removed default files

---
+++ src/components/ImageList.js
@@ -0,0 +1,9 @@
+function Image() {
+  return (
+    <div>
+      <h2>Image</h2>
+    </div>
+  );
+}
+
+export default Image;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+function SearchBar() {
+  return (
+    <div>
+      <h2>SearchBar</h2>
+    </div>
+  );
+}
+
+export default SearchBar;
